c1ee0cdbc3c8e623b5ea9238a4d327df342752c9
Carlo Echeverría <cecheverria@realmedianetwork.com>
2025-05-27 19:06:36 -0400

    Update revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import { getProfile } from "@/services/profileService"
 import { Project } from "@/types/project";
 import { Skill } from "@/types/skill"
 
-// export const revalidate = 86400;
+export const revalidate = parseInt(process.env.NEXT_PUBLIC_REVALIDATE_APP || '0');
 
 export default async function Home() {
   // Obtener datos del perfil
