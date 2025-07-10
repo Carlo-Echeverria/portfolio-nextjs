261a6ec2a77f67da956978ff346c8f4158c23f3c
Carlo Echeverría <cecheverria@realmedianetwork.com>
2025-07-10 14:50:11 -0400

    Remove stores

--- lib/store.ts
+++ lib/store.ts
@@ -4,14 +4,10 @@ interface StoreState {
   menuOpen: boolean
   toggleMenu: () => void
   closeMenu: () => void
-  activeSection: string
-  setActiveSection: (section: string) => void
 }
 
 export const useStore = create<StoreState>((set) => ({
   menuOpen: false,
   toggleMenu: () => set((state) => ({ menuOpen: !state.menuOpen })),
   closeMenu: () => set({ menuOpen: false }),
-  activeSection: "home",
-  setActiveSection: (section) => set({ activeSection: section }),
 }))
